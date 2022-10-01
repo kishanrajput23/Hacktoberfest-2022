685120063337cc8ad4f7a2eb46c1fc168e216b30
ironman32022 <106044015+ironman32022@users.noreply.github.com>
2022-10-01 19:59:17 +0530

    Add files via upload

---
+++ Cloned Projects/ironman32022/youtube_clone/index.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('#menu');
+console.log(menu);
+const sidebar = document.querySelector('.sidebar');
+console.log(sidebar);
+
+menu.addEventListener('click', function () {
+  sidebar.classList.toggle('show-sidebar');
+});
