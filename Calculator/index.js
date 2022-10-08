952d7821493b480c8dffdc32b95ef64968c43dcc
Omkar Khairnar <110295103+Omkar-Khairnar@users.noreply.github.com>
2022-10-08 10:55:11 +0530

    Omkar Calculator
    Calculator using HTML , CSS , JS

---
+++ Calculator/index.js
@@ -0,0 +1,22 @@
+
+
+let string = "";
+let buttons = document.querySelectorAll('.btn');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'AC'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+  
+      }
+  })
+})
